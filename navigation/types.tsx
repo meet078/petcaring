596e0a044dd34aeb1b64a6eee852416ef2352c50
meet078 <solankimeet078@gmail.com>
2023-03-06 23:41:31 +0530

    add navigaton type second time

--- navigation/types.tsx
+++ navigation/types.tsx
@@ -1,4 +1,4 @@
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export type RootStackParamList = {
